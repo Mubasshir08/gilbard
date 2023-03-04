1838b90b83329c3550092570a0fb1e271c73f066
Mubasshir08 <msr4212019@gmail.com>
2023-03-04 20:50:48 +0600

    newGames Section done not responsive

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import NewGames from './components/NewGames/NewGames';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
         <Header />
+        <NewGames />
     </div>
   );
 }
---
+++ src/components/NewGames/NewGames.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+// importing data
+import Data from '../../assets/JsonData/newGames.json';
+
+function NewGames(props) {
+    return (
+        <div>
+            <h1 className='text-[#252525] text-center text-lg'> <span className='text-[#061DA4]'> new </span> games</h1>
+            <section>
+                <div>
+                    {
+                        Data.map((game) => {
+                            if(game.row === 1){
+                                return <div className=''> 
+                                    <img src={require(`../../assets/images/${game.pic}`)} alt="gameImg" className='w-2/3 mx-auto my-5' />
+                                    <h3 className='text-xs ml-14 -mt-4'>{game.title}</h3>
+                                    <p className='text-[.65rem] text-right mr-14 -mt-4 text-[#061DA4]'>{game.os}</p>
+                                </div>
+                            }
+                        })
+                    }
+                 </div>
+                <div>
+                {
+                        Data.map((game) => {
+                            if(game.row === 2){
+                                return <div className='mb-10'> 
+                                    <img src={require(`../../assets/images/${game.pic}`)} alt="gameImg" className='w-2/3 mx-auto my-5' />
+                                    <h3 className='text-xs ml-14 -mt-4'>{game.title}</h3>
+                                    <p className='text-[.65rem] text-right mr-14 -mt-4 text-[#061DA4]'>{game.os}</p>
+                                </div>
+                            }
+                        })
+                    }
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default NewGames;
